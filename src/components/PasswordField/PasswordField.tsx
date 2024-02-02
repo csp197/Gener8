df83b84accadd8a3910a0e6f74e31d5ad0d01459
CSP <16367956+CSP197@users.noreply.github.com>
2024-02-02 08:58:14 -0800

    Fixing minor issue and adding assets

--- src/components/PasswordField/PasswordField.tsx
+++ src/components/PasswordField/PasswordField.tsx
@@ -30,12 +30,14 @@ const PasswordField = (props: IPasswordFieldProps) => {
       >
         Password
       </label> */}
-      <input
-        type="text"
+      {/* <input type="text" id="success" className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder={generatedPass} /> */}
+      <div
+        // type="text"
         id="success"
         className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
-        placeholder={generatedPass}
-      />
+      >
+        {generatedPass}
+      </div>
       {/* {password === "true" ? } */}
       <p className="mt-2 text-sm text-green-600 dark:text-green-500">
         {/* <span className="font-medium">This is secure!</span> */}
