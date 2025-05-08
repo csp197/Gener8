3dd887e70f6878f5e55bcb7cd38d9c2f7e082057
CSP <16367956+CSP197@users.noreply.github.com>
2025-05-07 23:12:55 -0700

    Updating yaml for gh deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
-  // "https://csp197.github.io/gener-8",
+  
+  // Set the base to the repository name for GitHub Pages
+  // If deploying to a custom domain or as a user/organization page, use '/'
+  base: '/gener-8/',
+  
   server: {
     port: 3000
   }
