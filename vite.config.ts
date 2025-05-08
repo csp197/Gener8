3cad5cec75b474867ed7653a5748560e4e17d26c
CSP <16367956+CSP197@users.noreply.github.com>
2025-05-07 23:34:27 -0700

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
-  // "https://csp197.github.io/gener-8",
+  base: "https://csp197.github.io/gener8",
   server: {
     port: 3000
   }
